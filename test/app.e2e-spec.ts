9954e37cdce5961411b861efd16fbb91102258de
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-12-17 18:15:08 +0900

    e2e test 작성 중

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          timezone: 'Asia/Seoul',
+          // synchronize: true,
+          synchronize: false,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  // user
+  it('/api/user/signUp (POST)', async () => {
+    const signUpDto = {
+      identification: 'testUser',
+      userType: '회원',
+      password: 'swim1234',
+      name: '홍길동',
+      gender: '남자',
+      birth: '1985.08.15',
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/api/user/signUp')
+      .send(signUpDto)
+      .expect(HttpStatus.OK);
+
+    expect(response.body.message).toBe('회원 가입 성공');
   });
 });
